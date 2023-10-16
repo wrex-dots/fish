122ed55036cf87a46ff52705b34f8a7872cf8807
Ludovic Fernandez <ludofernandez@msn.com>
2023-10-16 19:26:18 +0200

    chore: Remove unused i3-lock-fancy script

--- functions/lock.fish
+++
@@ -1,3 +0,0 @@
-function lock --description 'Lock the screen with a funny random commit message'
-	i3lock-fancy -g -f Fira-Code-Bold -t (printf "git commit -m '%s'" (lynx -nolist -dump whatthecommit.com | head -n -2 | xargs))
-end
