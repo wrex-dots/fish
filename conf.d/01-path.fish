d4d285f6ef305af1188145ca3aee46dfca76c865
Ludovic Fernandez <ludofernandez@msn.com>
2023-10-17 14:11:36 +0200

    refactor: Remove unneeded complexity in env setup

---
+++ conf.d/01-path.fish
@@ -0,0 +1,15 @@
+# Quick way to get here
+abbr -a fishenv $EDITOR (status -f)
+
+function addpath --description 'Extend PATH with new values'
+  for i in (seq (count $argv))
+    if not string match -q -- $argv[$i] $PATH && test -d $argv[$i]
+      set -p PATH $argv[$i]
+    end
+  end
+end
+
+addpath "$PNPM_HOME"
+
+addpath /opt/android-sdk/cmdline-tools/latest
+addpath /opt/android-sdk/platform-tools/
