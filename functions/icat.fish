e3d32cea8ee6f491ba96c42b9c5f973c73914bf4
Ludovic Fernandez <ludofernandez@msn.com>
2024-08-15 14:06:26 +0200

    refactor(functions/icat): Add `--wraps` directive

--- functions/icat.fish
+++ functions/icat.fish
@@ -1,3 +1,3 @@
-function icat --description 'alias icat kitty +kitten icat'
-  kitty +kitten icat $argv; 
+function icat --description 'alias icat kitty +kitten icat' --wraps 'kitty +kitten'
+  kitty +kitten icat $argv;
 end
