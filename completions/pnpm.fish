fe6583f1800cb7ea07b5f634fb38ab91c55226ac
Ludovic Fernandez <ludofernandez@msn.com>
2024-08-15 13:48:06 +0200

    chore: Update completions file for `pnpm`

--- completions/pnpm.fish
+++ completions/pnpm.fish
@@ -1,4 +1,23 @@
+###-begin-pnpm-completion-###
+function _pnpm_completion
+  set cmd (commandline -o)
+  set cursor (commandline -C)
+  set words (count $cmd)
 
-# tabtab source for pnpm package
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+  set completions (eval env DEBUG=\"" \"" COMP_CWORD=\""$words\"" COMP_LINE=\""$cmd \"" COMP_POINT=\""$cursor\"" SHELL=fish pnpm completion-server -- $cmd)
+
+  if [ "$completions" = "__tabtab_complete_files__" ]
+    set -l matches (commandline -ct)*
+    if [ -n "$matches" ]
+      __fish_complete_path (commandline -ct)
+    end
+  else
+    for completion in $completions
+      echo -e $completion
+    end
+  end
+end
+
+complete -f -d 'pnpm' -c pnpm -a "(_pnpm_completion)"
+###-end-pnpm-completion-###
+
